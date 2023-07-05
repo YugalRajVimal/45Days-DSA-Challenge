6029e84f3e3626f4ebb4cd1b29d4f23fc7b2f0a8
YugalRajVimal <sagaryugal472@gmail.com>
2023-07-05 15:51:18 +0530

    Search in Rotated Sorted Array

---
+++ 9-Trees/50-Search_in_rotated_sorted_array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums,int startIndex,int endIndex, int target){
+        if(startIndex>endIndex){
+            return -1;
+        }
+
+        int mid=(startIndex+endIndex)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            return binarySearch(nums,mid+1,endIndex,target);
+        }
+        return binarySearch(nums,startIndex,mid-1,target);
+    }
+
+
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int peakIndex=n-1;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>nums[i+1]){
+                peakIndex=i;
+                break;
+            }
+        }
+
+        if(peakIndex==n-1){
+            return binarySearch(nums,0,n-1,target);
+        }
+
+        if(target==nums[n-1]){
+            return n-1;
+        }
+        else if(target>nums[n-1]){
+            return binarySearch(nums,0,peakIndex,target);
+        }
+        return binarySearch(nums,peakIndex+1,n-1,target);
+    }
+};
